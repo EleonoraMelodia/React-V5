ce0c1735768f36059bc6b4920639035c1f724505
Eleonora Melodia <eleonoramelodia92@gmail.com>
2023-10-05 18:18:26 +0200

    exercise twelve

--- exercise-six/src/App.jsx
+++ exercise-six/src/App.jsx
@@ -1,10 +1,12 @@
 import { Hello } from "./Hello"
+import { Message } from "./Message"
 
 function App() {
  
   return (
     <div>
-     <Hello/>
+      <Hello />
+      <Message/>
     </div>
   )
 }
--- exercise-six/src/Hello.jsx
+++ exercise-six/src/Hello.jsx
@@ -2,7 +2,7 @@ export function Hello() {
   return (
     <>
       <h2> Hello, World! </h2>
-      <p>What a beautiful day!</p>
+      
     </>
-  );
+  )
 }
