1a57cf79786f829694a2dcbf0d7b4683258c60fe
Eleonora Melodia <eleonoramelodia92@gmail.com>
2023-10-06 10:21:31 +0200

    exercise eighteen modified

--- exercise-six/src/Welcome.jsx
+++ exercise-six/src/Welcome.jsx
@@ -7,4 +7,4 @@ export function Welcome({ name, age }) {
   );
 }
 
-/* It happens that the name is not rendered. If I set a default value this will be rendered, in case the name is missing in the parent file.  */
+
