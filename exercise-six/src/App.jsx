2afa59a7ebe4cd466ae0d45970ee8519122b9dcd
Eleonora Melodia <eleonoramelodia92@gmail.com>
2023-10-06 16:23:25 +0200

    exercise twenty nine done

--- exercise-six/src/App.jsx
+++ exercise-six/src/App.jsx
@@ -1,16 +1,24 @@
-import { AlertClock } from "./AlertClock";
 import { Hello } from "./Hello";
 import { Message } from "./Message";
 import { Welcome } from "./Welcome";
+import { AlertClock } from "./AlertClock";
+import { Counter } from "./Counter";
 
 function App() {
+  function handleButtonClick() {
+    const time = new Date();
+    alert(`The current time is: ${time.toLocaleTimeString()}`);
+  }
+
   return (
     <div>
       <Hello />
       <Message />
-      <Welcome name={<strong>John</strong>} age={4} />
+      <Welcome name={<strong>John</strong>} age={44} />
       <AlertClock buttonClicked={handleButtonClick} />
+      <Counter/>
     </div>
   );
 }
+
 export default App;
