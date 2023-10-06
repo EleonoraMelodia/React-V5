d21f8da05b59250cfd0099f8ad9be90b5a4373b8
Eleonora Melodia <eleonoramelodia92@gmail.com>
2023-10-06 18:58:40 +0200

    exercise ok

--- exercise-six/src/MouseClicker.jsx
+++ exercise-six/src/MouseClicker.jsx
@@ -1,10 +1,16 @@
 export function MouseClicker({ name }) {
   function handleClick(e) {
     console.log(e.target.name);
-  }
+    }
+    
+    function handleImg(e) {
+        console.log(e.target.src)
+    }
   return (
     <button name={name} onClick={handleClick}>
-      <img name = "image"  width={30} height={30} />
+      <img onClick={handleImg} name = "image" src="https://www.educolor.it/immagine-bottone-dl28074.jpg" width={45} height={30} />
     </button>
   );
 }
+
+/*yes the name is still printed in the console due to the event handler attached to the button, which however is triggered by the image */
