7adc83b6f22afbcd1a6123a7a207e139d1a96a6c
Eleonora Melodia <eleonoramelodia92@gmail.com>
2023-10-06 17:27:49 +0200

    exercise 32 ok

--- exercise-six/src/Counter.jsx
+++ exercise-six/src/Counter.jsx
@@ -1,7 +1,9 @@
+import { CounterDisplay } from "./CounterDisplay";
+
 export function Counter({counter, handleCounter}) {
     return (
         <div>
-            <h2>{counter}</h2>
+            <CounterDisplay count = {counter} />
             <button onClick={handleCounter}>Increment the number </button>
         </div>
     )
