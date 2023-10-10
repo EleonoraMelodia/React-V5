533179fa03351fbfc9ea832ce501ed92201b8663
Eleonora Melodia <eleonoramelodia92@gmail.com>
2023-10-10 10:45:05 +0200

    exrercise done

--- exercise-six/src/UncontrolledLogin.jsx
+++ exercise-six/src/UncontrolledLogin.jsx
@@ -1,6 +1,18 @@
 export function UncontrolledLogin() {
+    function handleFormSubmit(event) {
+        event.preventDefault();
+        const username = event.target.elements.namedItem('username').value;    
+        const password = event.target.elements.namedItem('password').value;
+        const data = {
+            username,
+            password
+        }
+        console.log(data);
+    }
+
+
   return (
-    <form action="#">
+    <form action="#" onSubmit={handleFormSubmit}>
       <input type="text" name="username" />
       <input type="password" name="password" />
       <input type="checkbox" name="remember" />
