0b361378b4c4a8ac81bf572762c522b2cbbe38cc
Eleonora Melodia <eleonoramelodia92@gmail.com>
2023-10-12 11:45:27 +0200

    modified

--- exercise-six/src/Welcome.jsx
+++ exercise-six/src/Welcome.jsx
@@ -3,8 +3,12 @@ import { Age } from "./Age";
 export function Welcome({ name, age }) {
   return (
     <>
-      <p className="paragraph">Welcome, {name}!  </p>
-      { 18 < age < 65 ? <Age age={age} /> : "too young or too old for this application"}
+      <p className="paragraph">Welcome, {name}! </p>
+      {18 < age < 65 ? (
+        <Age age={age} />
+      ) : (
+        "too young or too old for this application"
+      )}
     </>
   );
 }
