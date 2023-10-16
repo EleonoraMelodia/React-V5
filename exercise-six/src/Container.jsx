a17cbe70986a77f975039684b6c759da8ab2e4db
Eleonora Melodia <eleonoramelodia92@gmail.com>
2023-10-16 09:43:07 +0200

    exercise ok

--- exercise-six/src/Container.jsx
+++ exercise-six/src/Container.jsx
@@ -5,12 +5,13 @@ export function Container({ children, title }) {
   function handleCollapseButton() {
     setCollapsed((c) => !c);
   }
-
+    //con handle, inverto lo stato di collapsed tra true a false e viceversa. Quindi faccio si che questo parametro passi da true
+    //a false, e vicevera, in modo che con l' && operator, io possa mostrare o nascondere il contenuto
   return (
     <>
       {title}
       {collapsed && (
-        <div style={{ border: "1px solid blue", backgroundColor: "beige" }}>
+        <div style={{ border: "1px solid blue", backgroundColor: "beige" }}> 
           {children}
         </div>
       )}
