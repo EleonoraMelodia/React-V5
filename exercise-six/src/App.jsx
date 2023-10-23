485ad56fe740f21b08774ab31096b30d7072c2ee
Eleonora Melodia <eleonoramelodia92@gmail.com>
2023-10-23 13:09:34 +0200

    exercise 85 ok

--- exercise-six/src/App.jsx
+++ exercise-six/src/App.jsx
@@ -3,7 +3,7 @@ Add a new Route to the users/:username path that renders a ShowGithubUser
  component that receives the username as a path parameter and renders the 
  GithubUser component from the third exercise of Context by passing it the received username. */
 
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import { Welcome } from "./Welcome";
 import { Counter } from "./Counter";
 import ShowGithubUser from "./ShowGithubUser";
@@ -11,15 +11,19 @@ import ShowGithubUser from "./ShowGithubUser";
 const App = () => {
   return (
     <>
+      <nav>
+        <Link to="/"> Home </Link>
+        <Link to="/counter"> Counter </Link>
+        <Link to="/users/:username"> Users </Link>
+      </nav>
+
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/counter" element={<Counter />} />
-        <Route path="/users/:username" element={<ShowGithubUser/>} />
+        <Route path="/users/:username" element={<ShowGithubUser />} />
         <Route path="/:name" element={<Welcome />} />
       </Routes>
-
     </>
-
   );
 };
 
