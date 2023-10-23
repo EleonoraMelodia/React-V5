53e481fa3fbd6b3112d65b291f3fe93d5af337df
Eleonora Melodia <eleonoramelodia92@gmail.com>
2023-10-23 12:17:51 +0200

    exercise updated

--- exercise-six/src/App.jsx
+++ exercise-six/src/App.jsx
@@ -6,13 +6,14 @@
 
 import { Route, Routes } from "react-router-dom";
 import { Welcome } from "./Welcome";
-
+import {Counter} from "./Counter"
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/:name" element={<Welcome />} />
+        <Route path="/counter" element={< Counter/>} />
       </Routes>
     </>
   );
