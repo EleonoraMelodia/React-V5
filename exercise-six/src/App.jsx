709508fb34a1901440d511ad04983b5a6bc0cdeb
Eleonora Melodia <eleonoramelodia92@gmail.com>
2023-10-23 12:19:46 +0200

    exercise modified

--- exercise-six/src/App.jsx
+++ exercise-six/src/App.jsx
@@ -1,8 +1,4 @@
-/*Create an App component that wraps a Routes
- component and add a single Route to the / path that
- renders the Welcome component from the 
- first exercise of Styling Component, passing it a name prop.
-  Render the App component within a BrowserRouter component. */
+
 
 import { Route, Routes } from "react-router-dom";
 import { Welcome } from "./Welcome";
