4d829a7f66dab5d65eae778ce563bf2a0736ee7c
Eleonora Melodia <eleonoramelodia92@gmail.com>
2023-10-23 13:24:05 +0200

    exercise updated

--- exercise-six/src/App.jsx
+++ exercise-six/src/App.jsx
@@ -1,7 +1,3 @@
-/*84 - React Router
-Add a new Route to the users/:username path that renders a ShowGithubUser
- component that receives the username as a path parameter and renders the 
- GithubUser component from the third exercise of Context by passing it the received username. */
 
 import { Link, Route, Routes } from "react-router-dom";
 import { Welcome } from "./Welcome";
