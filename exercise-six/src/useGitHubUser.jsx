7edeafda5655ad12cef62af650b4cc19dc562b0f
Eleonora Melodia <eleonoramelodia92@gmail.com>
2023-10-25 10:28:30 +0200

    exercise ok

--- exercise-six/src/useGitHubUser.jsx
+++ exercise-six/src/useGitHubUser.jsx
@@ -1,38 +1,14 @@
-import { useState, useEffect } from "react";
+import useSWR from 'swr'
+
+const fetcher = url => fetch(url).then(response => response.json())
 
 const useGitHubUser = (username) => {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  async function fetchGithubUser() {
-    try {
-      setLoading(true);
-      const response = await fetch(`https://api.github.com/users/${username}`);
-
-      if (response.ok) {
-        const userData = await response.json();
-        setData(userData);
-        setError(null);
-      } else {
-        setError("An error occurred in the server call");
-      }
-    } catch (error) {
-      setError("An error occurred in your code", error);
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  useEffect(() => {
-    fetchGithubUser()
-  }, []);
-
+const{data, error} = useSWR(`https://api.github.com/users/${username}`, fetcher);
   return {
     data,
     error,
-    fetchGithubUser,
-    loading,
+    fetchGithubUser: fetcher,
+    loading: !data && !error,
   };
 };
 
